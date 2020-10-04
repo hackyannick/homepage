7f8cd80721bb733efcbd8fbdfde50b305c2896fd
hackyannick <hack.yannick@gmail.com>
2020-10-04 15:54:43 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+// Offset for Site Navigation
+$('#siteNav').affix({
+	offset: {
+		top: 100
+	}
+})
+/*
+     FILE ARCHIVED ON 10:15:26 Jan 21, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:34:20 Apr 26, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 548.48 (3)
+  esindex: 0.009
+  captures_list: 581.98
+  CDXLines.iter: 12.585 (3)
+  PetaboxLoader3.datanode: 573.316 (4)
+  exclusion.robots: 0.184
+  exclusion.robots.policy: 0.173
+  RedisCDXSource: 17.545
+  PetaboxLoader3.resolve: 34.81
+  load_resource: 93.244
+*/
