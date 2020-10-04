c789e85e57ec4d041a23e5323eb244852eb50ed5
hackyannick <hack.yannick@gmail.com>
2020-10-04 15:47:36 +0200

    Add files via upload

---
+++ inc/dyn_about.php
@@ -0,0 +1,22 @@
+ </div>
+    </div>
+  </nav>
+  <section class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6"></div>
+        <div class="col-sm-6">
+          <h2 class="section-header">Über mich</h2>
+          <img src="assets/pictures/me.jpg" width="150" height="200" alt=""/><br>
+          <br>
+          <p class="lead text-muted">Hallo, ich bin Yannick Hack und 22 Jahre alt, meine Hobbys sind sehr Weitreichend. Ich kann mich eigentlich für relativ viel </p>
+          <p>Begeistern. Dazu gehören ua. Retro PCs und die Apple Welt fasziniert mich ebenfalls. Ich habe schon Privat einige </p><br>
+          <p>iPhones Privat Repariert. Sowie mache ich im Bekannten oder Familienkreis den IT Support.
+		  </p><br>
+        </div>
+      </div>
+
+		
+	</center>
+		
+    </div>
