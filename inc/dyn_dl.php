ef538aaeaf2346353b2ed9059c07b464de57b765
hackyannick <hack.yannick@gmail.com>
2020-10-04 15:47:52 +0200

    Add files via upload

---
+++ inc/dyn_dl.php
@@ -0,0 +1,17 @@
+ </div>
+    </div>
+  </nav>
+  <section class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6"></div>
+        <div class="col-sm-6">
+          <h2 class="section-header">Downloads</h2><br>
+          <br>
+          <p class="lead text-muted"><a href="https://ftp.yannick-hack.de/dlsoft/t43_treiber.exe">ThinkPad T43 Treiberpack</a>
+		  <br>
+		  <p class="lead text-muted"><a href="https://ftp.yannick-hack.de/dlsoft/rdp51.exe">RDP 5.1 für neuere Windows Versionen.</a>
+        </div>
+      </div>
+	</center>
+    </div>
