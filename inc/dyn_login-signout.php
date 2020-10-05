037756203d1d1dcd6dfaf783f7a4c9ba05934225
hackyannick <hack.yannick@gmail.com>
2020-10-05 11:56:33 +0200

    Update dyn_login-signout.php

--- inc/dyn_login-signout.php
+++ inc/dyn_login-signout.php
@@ -1,4 +1,4 @@
- <meta http-equiv="refresh" content="0; URL=https://www.yannick-hack.de/logout_info.php">
+ <meta http-equiv="refresh" content="0; URL=logout_info.php">
 
     <iframe src="https://sso.yannick-hack.de/auth/realms/homepage/protocol/openid-connect/logout?https://www.yannick-hack.io/index.php" name="logout_helper" id="logout_helper" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="1" width="1">
 </iframe>
